ecdba3772a047055e6b666f370936a560e13b3f9
Helena <lenakaires@gmail.com>
2018-10-01 18:56:04 -0300

    Arrumando -fuck

---
+++ countdown.js
@@ -0,0 +1,14 @@
+var end_time = new Date("October 16, 2018").getTime();
+
+let timer_id = setInterval(function timer() {
+    var current_time = Date.now();
+    var wait_time = end_time - current_time;
+
+    document.getElementById("counter_minutes").innerHTML = Math.floor(((wait_time/1000)/60)%60);
+    document.getElementById("counter_hours").innerHTML = Math.floor((((wait_time/1000)/60)/60)%24);
+    document.getElementById("counter_days").innerHTML = Math.floor(((((wait_time/1000)/60)/60)/24));
+
+    if (wait_time === 0) {
+        clearInterval(timer_id);
+    }
+}, 1000);
